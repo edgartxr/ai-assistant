2e65dd2f1cde03aef5ce4f35f17eea980630727c
edgartxr <edgartxr@gmail.com>
2025-06-12 14:24:56 +0100

    Initial commit: Hello Streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="My First Cloud App")
+
+st.title("Hello Streamlit Community Cloud!")
+st.write("This is my first app deployed to the web.")
+st.success("It's working!")
